d3f68db8831a117a75df0f394b8bed0590b39c3f
Bartłomiej Kochanek <bkochanek4@gmail.com>
2024-02-23 22:23:31 +0100

    Change os check to match-case statement

--- code_tester/ui.py
+++ code_tester/ui.py
@@ -1,13 +1,14 @@
 import os
 
-if os.name == "nt":
-    import ctypes
+match os.name:
+    case "nt":
+        import ctypes
 
-    def msgbox(title: str, prompt: str):
-        ctypes.windll.user32.MessageBoxW(0, prompt, title, 0x1000)
+        def msgbox(title: str, prompt: str):
+            ctypes.windll.user32.MessageBoxW(0, prompt, title, 0x1000)
 
-else:
-    from tkinter import messagebox
+    case "posix":
+        from tkinter import messagebox
 
-    def msgbox(title: str, prompt: str):
-        messagebox.showinfo(title, prompt)
+        def msgbox(title: str, prompt: str):
+            messagebox.showinfo(title, prompt)
